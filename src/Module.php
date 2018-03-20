1ad44b1d72d2f91a889343cd6561671400dd8991
Marcel Candido <phpmarcel@gmail.com>
2018-03-20 09:26:37 -0300

    Commit Inicial

---
+++ src/Module.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace RabbitMQModule;
+
+use Zend\ModuleManager\ModuleManagerInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\EventManager\EventInterface;
+
+class Module
+{
+    public function onBootstrap(EventInterface $e)
+    {
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__,
+                ],
+            ],
+        ];
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+
+            ],
+        ];
+    }
+
+}
